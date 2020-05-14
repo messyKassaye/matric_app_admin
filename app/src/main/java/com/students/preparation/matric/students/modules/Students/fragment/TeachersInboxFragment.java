cd5e4dc8ff6dad8a3411887fd2a173ca40425de7
Sinkumen <sinkumen@gmail.com>
2020-05-14 15:14:31 +0300

    UI Update

---
+++ app/src/main/java/com/students/preparation/matric/students/modules/Students/fragment/TeachersInboxFragment.java
@@ -0,0 +1,61 @@
+package com.students.preparation.matric.students.modules.Students.fragment;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.students.preparation.matric.students.Constants;
+import com.students.preparation.matric.students.R;
+import com.students.preparation.matric.students.adapter.ReferenceBooksAdapter;
+import com.students.preparation.matric.students.model.Books;
+
+import java.util.ArrayList;
+
+
+public class TeachersInboxFragment extends Fragment {
+
+
+    public TeachersInboxFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view= inflater.inflate(R.layout.fragment_teachers_inbox, container, false);
+
+
+        return view;
+    }
+
+
+}
